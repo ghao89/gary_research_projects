a41256b76164f7b28af6239011868bd401038593
ghao89 <guanshengrui.hao@gmail.com>
2018-11-17 14:23:27 -0800

    Record the number of detections, true positive, false positive regions corresponding to different cutoff values.

--- dmrseq_investigation/dmrseq_on_simulated_data.R
+++ dmrseq_investigation/dmrseq_on_simulated_data.R
@@ -4,25 +4,38 @@
 
 library(dmrseq)
 
-load(file = "~/Dropbox/Private/Git_Projects/DLSMT/dmrseq_investigation/bs_dat.Rdata")
+alpha <- 0.05
+cutoff <- seq(0.01, 0.1, by = 0.01)
+fp_bef_merge <- numeric(length(cutoff))
+tp_merge <- numeric(length(cutoff))
+num_detect <- numeric(length(cutoff))
 
-n <- length(sampleNames(bs_dat))/2
 
-perms <- combn(2*n, n)
-i <- 1
-perm <- c(perms[, i], perms[, ncol(perms) - i + 1])
-p_data <- data.frame(cbind(c(rep("Control",n), rep("Treat", n))[perm],
-                           sampleNames(bs_dat)[perm]))
-colnames(p_data) <- c("Group", "Rep")
-pData(bs_dat) <- p_data
-
-t <- proc.time()
-dmrseq_dmr <- dmrseq(bs = bs_dat, 
-                     testCovariate = "Group",
-                     cutoff = 0.05,
-                     smooth = FALSE)
-proc.time() - t
-
-dmrseq_dmr$index[dmrseq_dmr$qval <= 0.05]
-
-intersect(dmrseq_dmr$index[dmrseq_dmr$qval <= 0.05], non_null_range)
+for (c in cutoff) {
+  idx <- which(cutoff == c)
+  load(file = "~/Dropbox/Private/Git_Projects/DLSMT/dmrseq_investigation/bs_dat.Rdata")
+  
+  n <- length(sampleNames(bs_dat))/2
+  
+  perms <- combn(2*n, n)
+  i <- 1
+  perm <- c(perms[, i], perms[, ncol(perms) - i + 1])
+  p_data <- data.frame(cbind(c(rep("Control",n), rep("Treat", n))[perm],
+                             sampleNames(bs_dat)[perm]))
+  colnames(p_data) <- c("Group", "Rep")
+  pData(bs_dat) <- p_data
+  
+  t <- proc.time()
+  dmrseq_dmr <- dmrseq(bs = bs_dat, 
+                       testCovariate = "Group",
+                       cutoff = c,
+                       smooth = FALSE)
+  proc.time() - t
+  
+  detected <- dmrseq_dmr$index[dmrseq_dmr$qval <= alpha]
+  num_detect[idx] <- length(detected)
+  tp_bef_merge <- intersect(dmrseq_dmr$index[dmrseq_dmr$qval <= alpha], non_null_range)
+  tp_merge[idx] <- sum(unique(floor(start(tp_bef_merge)/100)) %in% (non_null_idx - 1))
+  fp_bef_merge[idx] <- length(detected) - length(tp_bef_merge)
+  
+}
